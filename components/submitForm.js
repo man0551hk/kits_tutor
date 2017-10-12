856705cafee39967195a62df003d0dadcbc118d0
Jerry Wong <jerry.wong@iproperty.com>
2017-10-12 16:45:57 +0800

    update

--- components/submitForm.js
+++ components/submitForm.js
@@ -1,16 +1,71 @@
 import React, { Component } from 'react'
-import { StyleSheet, Text, ScrollView, View, TouchableOpacity, TextInput } from 'react-native'
+import { StyleSheet, Text, ScrollView, View, TouchableOpacity } from 'react-native'
 import { Container, Header, Content, Form, Item, Input, Label, Button} from 'native-base'
 
 export default class SubmitForm extends Component {
   constructor (props) {
     super(props)
+    this.state = {
+      mobile: '',
+      tutor_name: '',
+      brief: '',
+      request_salary: '',
+      monday: '',
+      tuesday: '',
+      wednesday: '',
+      thursday: '',
+      friday: '',
+      saturday: '',
+      sunday:''
+    }
+  }
+
+  sendRequest() {
+    console.log('a')
+    // var details = {
+    //   'job_number': this.props.job_number,
+    //   'case_content1': this.props.job_district + ' ' + this.props.job_address + this.props.job_stu_level + ' ' + this.props.job_stu_sex + ' ' + 
+    //   this.props.job_stu_subject + ' ' + this.props.job_time + ' $' + this.props.job_hourrate + ' ' + this.props.job_rate_type,
+    //   'mobile': this.mobile,
+    //   'tutor_name': this.tutor_name,
+    //   'brief': this.brief,
+    //   'request_salary':  this.request_salary,
+    //   'monday': this.monday,
+    //   'tuesday': this.tuesday,
+    //   'wednesday': this.wednesday,
+    //   'thursday': this.thursday,
+    //   'friday': this.wednesfridayay,
+    //   'saturday': this.saturday,
+    //   'sunday': this.sunday
+    // }
+    // console.log(details)
+    // var formBody = []
+    // for (var property in details) {
+    //   var encodedKey = encodeURIComponent(property)
+    //   var encodedValue = encodeURIComponent(details[property])
+    //   formBody.push(encodedKey + "=" + encodedValue)
+    // }
+    // formBody = formBody.join("&")
+    // console.log(formBody)
+    
+    // fetch('http://localhost/job/tutorSumitJob1.php', {
+    //   method: 'POST',
+    //   headers: {
+    //     'Accept': 'application/json',
+    //     'Content-Type': 'application/x-www-form-urlencoded'
+    //   },
+    //   body: formBody
+    // }).then(
+    //   response => response.text()
+    // ).then((response) => {
+    //   alert(response)
+    // }).done()
   }
 
   render () {
     return (
       <Content style={styles.container}>
-        <View style={{paddingLeft:14}}>
+        <View style={{paddingLeft:14, paddingTop: 20}}>
           <Text style={styles.bold}>
             個案內容
           </Text>
@@ -38,29 +93,25 @@ export default class SubmitForm extends Component {
           <Item>
             <Input
               keyboardType='phone-pad'
-              ref= {(el) => { this.mobile = el; }} onChangeText={(mobile) => this.setState({mobile: mobile})}
-              value={this.state.mobile}
+              onChangeText={mobile => this.setState({mobile: mobile})}
               style={styles.input}
               placeholder='手提電話' />
           </Item>
           <Item>
             <Input
-              //onChangeText={(text) => this.saveMobile(text)}
-              value={this.state.mobile}
+              onChangeText={tutor_name => this.setState({tutor_name: tutor_name})}
               style={styles.input}
               placeholder='導師姓名' />
           </Item>
           <Item>
             <Input
-              //onChangeText={(text) => this.saveMobile(text)}
-              value={this.state.mobile}
+              onChangeText={brief => this.setState({brief: brief})}
               style={styles.input}
               placeholder='個人簡介'  multiline={true} style={{height:200}}/>
           </Item>   
           <Item>
             <Input
-              //onChangeText={(text) => this.saveMobile(text)}
-              value={this.state.mobile}
+              onChangeText={request_salary => this.setState({request_salary: request_salary})}
               style={styles.input}
               placeholder='要求薪金'/>
           </Item>
@@ -69,58 +120,52 @@ export default class SubmitForm extends Component {
           </Text>  
           <Item>
             <Input
-              //onChangeText={(text) => this.saveMobile(text)}
-              value={this.state.mobile}
+              onChangeText={monday => this.setState({monday: monday})}
               style={styles.input}
               placeholder='星期一'/>
           </Item>       
           <Item>
             <Input
-              onChangeText={(text) => this.saveMobile(text)}
-              value={this.state.mobile}
+              onChangeText={tuesday => this.setState({tuesday: tuesday})}
               style={styles.input}
               placeholder='星期二'/>
           </Item>     
           <Item>
             <Input
-              //onChangeText={(text) => this.saveMobile(text)}
-              value={this.state.mobile}
+              onChangeText={wednesday => this.setState({wednesday: wednesday})}
               style={styles.input}
               placeholder='星期三'/>
           </Item>     
           <Item>
             <Input
-              //onChangeText={(text) => this.saveMobile(text)}
-              value={this.state.mobile}
+            onChangeText={thursday => this.setState({thursday: thursday})}
               style={styles.input}
               placeholder='星期四'/>
           </Item>     
           <Item>
             <Input
-              //onChangeText={(text) => this.saveMobile(text)}
-              value={this.state.mobile}
+            onChangeText={friday => this.setState({friday: friday})}
               style={styles.input}
               placeholder='星期五'/>
           </Item>     
           <Item>
             <Input
-              //onChangeText={(text) => this.saveMobile(text)}
-              value={this.state.mobile}
+              onChangeText={saturday => this.setState({saturday: saturday})}
+              value={this.state.saturday}
               style={styles.input}
               placeholder='星期六'/>
           </Item>     
           <Item>
             <Input
-              //onChangeText={(text) => this.saveMobile(text)}
-              value={this.state.mobile}
+            onChangeText={sunday => this.setState({sunday: sunday})}
               style={styles.input}
               placeholder='星期日'/>
           </Item>    
                                                                                       
         </Form>
-        <View style={{paddingLeft:17, paddingRight:17, paddingTop:10}}>
-        <Button block primary ><Text>應徵</Text></Button>
-        </View>   
+        <View style={{paddingLeft:17, paddingRight:17, paddingTop:10, paddingBottom: 30}}>
+          <Button block primary onPress={() => alert('a')}><Text>應徵</Text></Button>
+        </View>
       </Content>
     )
   }
@@ -128,7 +173,7 @@ export default class SubmitForm extends Component {
 
 const styles = StyleSheet.create({
   container: {
-
+    
   },
   bold: {
     fontWeight: 'bold',
